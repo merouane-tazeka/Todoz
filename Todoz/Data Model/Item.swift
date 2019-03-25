51546a1c94edd31031e045297054fad658f15193
Merouane TAZEKA <merouane.tazeka@gmail.com>
2019-03-25 10:34:43 -0400

    Convert to MVC design pattern.

---
+++ Todoz/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoz
+//
+//  Created by Merouane Tazeka on 2019-03-22.
+//  Copyright © 2019 Merouane Tazeka. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
